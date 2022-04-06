f8c6d62ff70b113839d77a0a33a1304c4a06341e
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-06 21:51:47 +0600

    Update App.jsx

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 
+const Header = React.lazy(() => import("home/Header"));
 import Footer from "home/Footer";
-import Header from "home/Header";
-
 import "./index.scss";
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <Header/>
+    <Suspense fallback={<div>Loading</div>}>
+      <Header />
+    </Suspense>
     <div className="my-10">PDP Page Content</div>
-    <Footer/>
+    <Footer />
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
