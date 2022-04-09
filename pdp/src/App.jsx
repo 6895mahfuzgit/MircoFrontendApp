5017d03da16f399a04e4f2da5a35b2fa92b08474
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-09 12:46:09 +0600

    Restore Previous Version Code.

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,28 +1,18 @@
-import React, { Suspense, useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
 import Footer from "home/Footer";
-//import Header from "home/Header";
-const Header=React.lazy(()=>import("home/Header") )
+import Header from "home/Header";
 
 import "./index.scss";
 
-const App = () => {
-  const [showHeader, setshowHeader] = useState(false);
-
-  return (
-    <div className="mt-10 text-3xl mx-auto max-w-6xl">
-      { showHeader &&
-        (
-          <Suspense fallback={<div>Loading......</div>}>
-          <Header />
-        </Suspense>
-        )
-      }
-      <button className="text-3xl p-5" onClick={()=>setshowHeader(!showHeader)} >Show the header</button>
-      <div className="my-10">PDP Page Content</div>
-      <Footer />
-    </div>
-  );
+const App = () =>{
+ return  (
+  <div className="mt-10 text-3xl mx-auto max-w-6xl">
+    <Header/>
+    <div className="my-10">PDP Page Content</div>
+    <Footer/>
+  </div>
+);
 };
 ReactDOM.render(<App />, document.getElementById("app"));
