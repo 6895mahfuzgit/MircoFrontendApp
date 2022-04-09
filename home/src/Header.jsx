77cb93547699886b4f6ba10f8874063513e54639
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-09 13:07:42 +0600

    Adding App Name On Header Component In Home App

--- home/src/Header.jsx
+++ home/src/Header.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function Header(){
+export default function Header({app}){
     return(
         <div className="p-5 bg-blue-500 text-white text-3x1 font-blod" >
-           Fidget Spinner World
+           Fidget Spinner World {app.name}
         </div>
     );
 }
