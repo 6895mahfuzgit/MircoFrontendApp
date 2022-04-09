b9b0992e99ee218a71bdc7b76e69f6432903f277
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-09 13:16:17 +0600

    Cache  Component ERROR In  PDP App

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -5,14 +5,17 @@ import Footer from "home/Footer";
 import Header from "home/Header";
 
 import "./index.scss";
+import SafeComponent from "./SafeComponent";
 
-const App = () =>{
- return  (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <Header/>
-    <div className="my-10">PDP Page Content</div>
-    <Footer/>
-  </div>
-);
+const App = () => {
+  return (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+      <SafeComponent>
+        <Header />
+      </SafeComponent>
+      <div className="my-10">PDP Page Content</div>
+      <Footer />
+    </div>
+  );
 };
 ReactDOM.render(<App />, document.getElementById("app"));
