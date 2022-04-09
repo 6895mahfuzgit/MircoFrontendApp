95e651dec4a7d7d2fba32d5083f14cc7d1436450
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-09 13:20:06 +0600

    Update Header.jsx

--- home/src/Header.jsx
+++ home/src/Header.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function Header({app}){
+export default function Header(){
     return(
         <div className="p-5 bg-blue-500 text-white text-3x1 font-blod" >
-           Fidget Spinner World {app.name}
+           Fidget Spinner World 
         </div>
     );
 }
