d83fa152eb0f45d74b93ba0e9487e7d5596ad592
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-09 12:42:32 +0600

    Lazy Loading In React Using State.

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { Suspense, useState } from "react";
 import ReactDOM from "react-dom";
 
 import Footer from "home/Footer";
-import Header from "home/Header";
+//import Header from "home/Header";
+const Header=React.lazy(()=>import("home/Header") )
 
 import "./index.scss";
 
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <Header/>
-    <div className="my-10">PDP Page Content</div>
-    <Footer/>
-  </div>
-);
+const App = () => {
+  const [showHeader, setshowHeader] = useState(false);
+
+  return (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+      { showHeader &&
+        (
+          <Suspense fallback={<div>Loading......</div>}>
+          <Header />
+        </Suspense>
+        )
+      }
+      <button className="text-3xl p-5" onClick={()=>setshowHeader(!showHeader)} >Show the header</button>
+      <div className="my-10">PDP Page Content</div>
+      <Footer />
+    </div>
+  );
+};
 ReactDOM.render(<App />, document.getElementById("app"));
