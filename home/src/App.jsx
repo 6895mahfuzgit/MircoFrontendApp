a87dc3bf42de377a17535630a10e1ef773b6717e
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-09 13:20:09 +0600

    Update App.jsx

--- home/src/App.jsx
+++ home/src/App.jsx
@@ -8,7 +8,7 @@ import "./index.scss";
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <Header app={{ "name":"home"}}/>
+    <Header />
     <div className="my-10">Home Page Content</div>
     <Footer/>
   </div>
