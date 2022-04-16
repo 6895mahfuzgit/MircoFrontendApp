952330d0cd24436e0b21d35d380bd9fa9279e9b0
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-17 01:06:49 +0600

    create initial Cart and assign Initial Cart.

--- serverApp/src/modules/cart/cart.controller.ts
+++ serverApp/src/modules/cart/cart.controller.ts
@@ -1,4 +1,4 @@
-import { Product } from './../../products';
+import products, { Product } from './../../products';
 import {
   Controller,
   Get,
@@ -16,15 +16,29 @@ interface Cart {
   cartItems: CartItem[];
 }
 
-
+const initialCart=(indexes:number[]):Cart=>({
+  cartItems: indexes.map((index)=>({
+    ...products[index],
+    quantity: 1,
+  }))
+});
 
 @Controller('cart')
 export class CartController {
+  
+  private carts:Record<number,Cart>={
+
+    1:initialCart([0,2,4]),
+    2:initialCart([1,3]),
+  }
+
   constructor() { }
 
   @Get()
   @UseGuards(JwtAuthGuard)
-  async index(@Request() req): Promise<{ userId: number }> {
-    return { userId: req.user.userId };
+  async index(@Request() req): Promise<Cart> {
+    //return { userId: req.user.userId };
+
+    return this.carts[req.user.userId]??{cartItems:[]};
   }
 }
