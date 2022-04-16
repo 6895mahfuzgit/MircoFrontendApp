ee381245074c6420b3f63eb466420f8d12108138
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-17 00:50:22 +0600

    Created Cart  Controller  and Module. Register Cart Module in App Module

--- serverApp/src/app.module.ts
+++ serverApp/src/app.module.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 
-import { AuthorizedModule } from './modules/authorized/authorized.module';
+import { CartModule } from './modules/cart/cart.module';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { UsersService } from './users/users.service';
@@ -16,7 +16,7 @@ import { ProductsModule } from './modules/products/products.module';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    AuthorizedModule,
+    CartModule,
     ProductsModule,
     AuthModule,
   ],
