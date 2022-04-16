df978dea87f26dcdc8a71183b3e245e14ed89edf
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-16 14:09:05 +0600

    Added React Routing in App Component

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -4,21 +4,30 @@ import ReactDOM from "react-dom";
 import Footer from "home/Footer";
 import Header from "home/Header";
 import PDPContent from "./PDPContent";
+import { BrowserRouter , Route, Routes, Switch } from "react-router-dom";
 
 import "./index.scss";
 import SafeComponent from "./SafeComponent";
 
 const App = () => {
   return (
-    <div className="mt-10 text-3xl mx-auto max-w-6xl">
-      <SafeComponent>
-        <Header />
-      </SafeComponent>
-      <div className="my-10">
-        <PDPContent />
+    <BrowserRouter>
+    
+      <div className="mt-10 text-3xl mx-auto max-w-6xl">
+        <SafeComponent>
+          <Header />
+        </SafeComponent>
+        <div className="my-10">
+           
+        <Routes>
+            <Route path="/product/:id" component={PDPContent} />
+            </Routes>     
+          
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+      
+    </BrowserRouter>
   );
 };
 ReactDOM.render(<App />, document.getElementById("app"));
