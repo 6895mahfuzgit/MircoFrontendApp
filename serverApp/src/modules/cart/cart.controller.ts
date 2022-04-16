9d838e1f37ec32003a804ba668f5a336ae52acd6
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-17 00:54:20 +0600

    created CartItem ansd Cart  interface

--- serverApp/src/modules/cart/cart.controller.ts
+++ serverApp/src/modules/cart/cart.controller.ts
@@ -1,3 +1,4 @@
+import { Product } from './../../products';
 import {
   Controller,
   Get,
@@ -6,13 +7,24 @@ import {
 } from '@nestjs/common';
 import { JwtAuthGuard } from '../../auth/jwt-auth.guard';
 
+
+interface CartItem extends Product {
+  quantity: number;
+}
+
+interface Cart {
+  cartItems: CartItem[];
+}
+
+
+
 @Controller('cart')
 export class CartController {
-  constructor() {}
+  constructor() { }
 
   @Get()
   @UseGuards(JwtAuthGuard)
-  async index(@Request() req): Promise<{userId: number}> {
+  async index(@Request() req): Promise<{ userId: number }> {
     return { userId: req.user.userId };
   }
 }
