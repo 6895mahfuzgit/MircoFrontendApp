bb6425a5d645af9835627295d081880ac2dcfd2b
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-17 10:12:24 +0600

    Created Cart Create and Delete API in Server App

--- serverApp/src/modules/cart/cart.controller.ts
+++ serverApp/src/modules/cart/cart.controller.ts
@@ -1,7 +1,10 @@
 import products, { Product } from './../../products';
 import {
+  Body,
   Controller,
+  Delete,
   Get,
+  Post,
   Request,
   UseGuards,
 } from '@nestjs/common';
@@ -16,8 +19,8 @@ interface Cart {
   cartItems: CartItem[];
 }
 
-const initialCart=(indexes:number[]):Cart=>({
-  cartItems: indexes.map((index)=>({
+const initialCart = (indexes: number[]): Cart => ({
+  cartItems: indexes.map((index) => ({
     ...products[index],
     quantity: 1,
   }))
@@ -25,11 +28,11 @@ const initialCart=(indexes:number[]):Cart=>({
 
 @Controller('cart')
 export class CartController {
-  
-  private carts:Record<number,Cart>={
 
-    1:initialCart([0,2,4]),
-    2:initialCart([1,3]),
+  private carts: Record<number, Cart> = {
+
+    1: initialCart([0, 2, 4]),
+    2: initialCart([1, 3]),
   }
 
   constructor() { }
@@ -39,6 +42,34 @@ export class CartController {
   async index(@Request() req): Promise<Cart> {
     //return { userId: req.user.userId };
 
-    return this.carts[req.user.userId]??{cartItems:[]};
+    return this.carts[req.user.userId] ?? { cartItems: [] };
   }
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  async createx(@Request() req, @Body() { id }: { id: string }): Promise<Cart> {
+    const productId = parseInt(id);
+    const cart = this.carts[req.user.userId];
+    const cartItem = cart.cartItems.find((cartItem) => cartItem.id === productId);
+
+    if (cartItem) {
+      cartItem.quantity += 1;
+    } else {
+      cart.cartItems.push({
+        ...products.find((product) => product.id == productId),
+        quantity: 1
+      });
+    }
+
+    return cart;
+  }
+
+  @Delete()
+  @UseGuards(JwtAuthGuard)
+  async destory(@Request() req): Promise<Cart> {
+    this.carts[req.user.userId] = { cartItems: [] };
+    return this.carts[req.user.userId];
+  }
+
+
 }
