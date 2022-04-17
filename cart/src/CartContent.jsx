b5b74bb3d0c72e1139375d8c9a2c4dcece315445
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-17 23:06:33 +0600

    Update CartContent.jsx

--- cart/src/CartContent.jsx
+++ cart/src/CartContent.jsx
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from "react";
 
 import {jwt,login} from "./cart.js";
 import Login from "./Login.jsx";
+import MiniCart from "./MiniCart.jsx";
 
 export default function CartContent() {
 
     const[token,setToken]=useState("");
    
     return <div>
-        <div><Login/></div>
-        JWT:{token}</div>
+        <div><Login/>
+        <MiniCart/>
+        JWT:{token}</div></div>
 
 }
