ebaa0fbc18ffd9e6d972a722af9762e9192e07f6
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-17 23:06:28 +0600

    Update cart.js

--- cart/src/cart.js
+++ cart/src/cart.js
@@ -3,6 +3,36 @@ import { BehaviorSubject } from "rxjs";
 
 const API_SERVER = "http://localhost:8080";
 export const jwt = new BehaviorSubject(null);
+export const cart = new BehaviorSubject(null);
+
+
+export const getCart = () =>
+  fetch(`${API_SERVER}/cart`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${jwt.value}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      cart.next(res);
+      console.log(res);
+      return res;
+    });
+
+
+    export const clearCart = () =>
+    fetch(`${API_SERVER}/cart`, {
+     method:"DELETE", 
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${jwt.value}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+          getCart();
+      });    
 
 export const login = (username, password) =>
     fetch(`${API_SERVER}/auth/login`, {
