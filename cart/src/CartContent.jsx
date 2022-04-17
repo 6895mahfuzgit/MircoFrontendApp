9d4c62ef3aaf14c66aa02b083d525b0c6540d8c5
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-17 14:20:44 +0600

    Update CartContent.jsx

--- cart/src/CartContent.jsx
+++ cart/src/CartContent.jsx
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from "react";
 
+import {jwt,login} from "./cart.js";
 
 export default function CartContent() {
 
     const[token,setToken]=useState("");
-    useEffect(()=>{},[]);
+    useEffect(()=>{
+        login("sally","123")
+     return jwt.subscribe((val)=>setToken(val??""))
+    },[]);
+
     return <div>JWT:{token}</div>
 
 }
