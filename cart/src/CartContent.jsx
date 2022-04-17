da4fb51cfff5aec4c21b0c4118019076474b3b45
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-17 14:54:00 +0600

    created login and use use in cart content

--- cart/src/CartContent.jsx
+++ cart/src/CartContent.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 
 import {jwt,login} from "./cart.js";
+import Login from "./Login.jsx";
 
 export default function CartContent() {
 
     const[token,setToken]=useState("");
    
-    return <div>JWT:{token}</div>
+    return <div>
+        <div><Login/></div>
+        JWT:{token}</div>
 
 }
