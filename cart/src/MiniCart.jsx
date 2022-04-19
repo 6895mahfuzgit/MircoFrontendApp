963df9543f30191ad05ab1ac608fd7e83f28ae8e
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-19 18:02:51 +0600

    Update MiniCart.jsx

--- cart/src/MiniCart.jsx
+++ cart/src/MiniCart.jsx
@@ -32,6 +32,7 @@ export default function MiniCart(){
               style={{
                 width: 300,
                 top: "2rem",
+                left: -250,
               }}
             >
               <div
