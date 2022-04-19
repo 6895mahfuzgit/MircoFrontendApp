9e2f61fdd76b5837e46bb5bf976da1504a1ab8f3
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-19 18:02:56 +0600

    Update HomeContent.jsx

--- home/src/HomeContent.jsx
+++ home/src/HomeContent.jsx
@@ -1,32 +1,47 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { currency, getProducts } from "./products";
 
+import { addToCart, useLoggedIn } from "cart/cart";
 
 
-export default function HomeContent(){
+export default function HomeContent() {
 
-    const [products,setProducts]=useState([]);
+    const loggedin = useLoggedIn();
+    const [products, setProducts] = useState([]);
 
-    useEffect(()=>{
-       getProducts().then(setProducts);
-    },[]);
+    useEffect(() => {
+        getProducts().then(setProducts);
+    }, []);
 
-    return(
+    return (
         <div className="my-10 grid grid-cols-4 gap-5" >
-           {
-               products.map((product)=>(
-                   <div key={product.id}>
-                       <img src={product.image} alt={product.name} />
-                       <div className="flex">
-                           <div className="flex-grow font-bold">
-                             <a>{product.name}</a>
-                           </div>
-                           <div className="flex-end">{currency.format(product.price)}</div>
-                       </div>
-                       <div className="text-sm mt-4">{product.description}</div>
-                   </div>
-               ))
-           }
+            {
+                products.map((product) => (
+                    <div key={product.id}>
+                        <img src={product.image} alt={product.name} />
+                        <div className="flex">
+                            <div className="flex-grow font-bold">
+                                <a>{product.name}</a>
+                            </div>
+                            <div className="flex-end">{currency.format(product.price)}</div>
+                        </div>
+                        <div className="text-sm mt-4">{product.description}</div>
+                        {
+                            loggedin &&
+                            (
+                                <div className="text-right mt-2">
+                                    <button
+                                        className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-2 px-4 rounded"
+                                        onClick={() => addToCart(product.id)}
+                                        id={`addtocart_${product.id}`}
+                                    >
+                                        Add to Cart
+                                    </button>
+                                </div>
+                            )}
+                    </div>
+                ))
+            }
         </div>
     );
 }
